ddc67c6f2a9b5038f7dfcdaf16d42e51e4e70eea
Андрей Викторович Родионов <roandr1970@localhost>
2021-09-10 12:50:42 +0300

    добавление класса PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] postes;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset ) {
+
+        return null;
+    }
+
+    public void delete(int ownerID, int postId) {
+
+    }
+}
